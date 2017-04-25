98116f0368b4d3f950ebf42853044785f3ec304b
guosheng <1535233734@.com>
2017-04-25 17:58:44 +0800

    mytopo部署

---
+++ js/myTopo.js
@@ -0,0 +1,13 @@
+//调用阶段-数据去调用
+
+
+
+//部署阶段
+function myTopoReady(){
+	var str="<article>"
+		+"<hgroup uid='myTopo-number'></hgroup>"
+		+"<canvas uid='myTopo-canvas'></canvas>"
+	+"</article>"
+	$('body').append(str)
+}
+//数据加载阶段
