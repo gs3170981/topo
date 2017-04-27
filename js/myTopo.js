40364fbf0b5bc22f8241b1415a5ec366b7cfd126
guosheng <1535233734@.com>
2017-04-27 18:02:33 +0800

    箭头没做

--- js/myTopo.js
+++ js/myTopo.js
@@ -19,20 +19,38 @@ var data={
 			y:200,
 			src:'img/3.jpg',
 			txt:'123123123'
+		},{
+			key:'D',
+			x:300,
+			y:300,
+			src:'img/3.jpg',
+			txt:'123123123'
 		}
 	],
 	nodeLinkInfo:[
 		{
 			fromKey:'A',
 			toKey:'B',
-			totalCount:147
+			totalCount:10000
 		},{
 			fromKey:'B',
 			toKey:'C',
 			totalCount:150
 		},{
-			fromKey:'C',
-			toKey:'A',
+			fromKey:'A',
+			toKey:'C',
+			totalCount:100
+		},{
+			fromKey:'A',
+			toKey:'D',
+			totalCount:100
+		},{
+			fromKey:'D',
+			toKey:'C',
+			totalCount:100
+		},{
+			fromKey:'D',
+			toKey:'B',
 			totalCount:100
 		}
 	]
@@ -42,59 +60,103 @@ function myTopoCreate(data){
 	ready();
 	createDiv(data.nodeInfo);
 	createLine(data.nodeLinkInfo,data.nodeInfo);
+	createNumber(data.nodeLinkInfo);
 	//部署阶段-DOM父级创建
 	function ready(){
 		var articleKlass="width:700px;height:500px;position:relative;margin:0 auto;border:1px solid red;";
 		var hgroupKlass="position:absolute;width:100%;height:100%;";
 		var str="<article style='"+articleKlass+"' >"
 			+"<hgroup uid='myTopo-number' style='"+hgroupKlass+"z-index:1'></hgroup>"
-			+"<canvas uid='myTopo-canvas' style="+hgroupKlass+"></canvas>"
+			+"<canvas uid='myTopo-canvas' height='500' width='700' style='position:absolute;'></canvas>"
 		+"</article>"
 		$('body').append(str);//后期修改位置
 	}
 	//模块创建
 	function createDiv(data){
-		var fatDiv="padding:10px 20px;width:80px;text-align:center;position:absolute;cursor:pointer;box-shadow:0 0 3rem #b7c0c1;";
+		var fatDiv="padding:10px 20px;width:80px;text-align:center;position:absolute;cursor:pointer;box-shadow:0 0 3rem #b7c0c1;border-radius:10px;background:white;";
 		var img="height: 50px;width: 50px;";
 		var txt="word-break:break-all;width:200px;overflow:auto;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;";
 		for(var i=0;i<data.length;i++){
-			var str="<div uid="+data[i].key+" style='"+fatDiv+"top:"+data[i].y+"px;left:"+data[i].x+"px;' onmousedown='myTopoDrag(event,this)'>"
+			var str="<div uid="+data[i].key+" style='"+fatDiv+"top:"+data[i].y+"px;left:"+data[i].x+"px;' onmousedown="+"myTopoDrag(event,this)"+" >"
 				+"<div style='width: 100%;'><img draggable='false' src="+data[i].src+" style='"+img+"' /></div>"
 				+"<small style='"+txt+"'>"+data[i].txt+"</small>"
 			+"</div>";
 			$('hgroup[uid="myTopo-number"]').append(str);
 		}
 	}
+	//number创建
+	function createNumber(data){
+		var klass="position:absolute;";
+		for(var i=0;i<data.length;i++){
+			var objMid=mid(lineSubscript(data[i].fromKey),lineSubscript(data[i].toKey));
+			var str="<small uid="+(data[i].fromKey+data[i].toKey)+" style='"+klass+"top:"+objMid.y+"px;left:"+objMid.x+"px;'>"+data[i].totalCount+"</small>"
+			$('hgroup[uid="myTopo-number"]').append(str);
+			//输出后校正，否则获取不到宽高
+			numberSubscript(data[i]);
+		}
+		//number坐标校正
+		function numberSubscript(str){
+			var obj=$("small[uid='"+(str.fromKey+str.toKey)+"']");
+			var obj_w=$(obj).width();
+			var obj_h=$(obj).height();
+			var obj_t=$(obj).position().top;
+			var obj_l=$(obj).position().left;
+			$(obj).css('left',obj_l-obj_w/2);
+			$(obj).css('top',obj_t-obj_h/2);
+		}
+	}
 }
-//line创建
-function createLine(data,dom){
+//取直线中点
+function mid(fromKey,toKey){
+	var data={};
+	data.x=(fromKey.x+toKey.x)/2;
+	data.y=(fromKey.y+toKey.y)/2;
+	return data;
+}
+//canvas-line创建
+function createLine(data,dom,sel){
 	var obj=$("canvas[uid='myTopo-canvas']")[0];
 	var cav=obj.getContext("2d");
-    cav.lineWidth = 1;
-	for(var i=0;i<data.length;i++)createLineChild(data[i].fromKey,data[i].toKey,data[i].totalCount)
+	var cache=0;
+    cav.lineWidth=1;
+    cav.strokeStyle='rgba(83,6,155,0.2)';
+    cav.clearRect(0,0,obj.width,obj.height);//清除重绘
+	for(var i=0;i<data.length;i++){
+		if(sel)if(cache<sel.length&&i==sel[cache]){
+	    	cav.strokeStyle='rgba(83,6,155,1)';
+	    	cache++;
+	   	}else cav.strokeStyle='rgba(83,6,155,0.2)';
+		createLineChild(data[i].fromKey,data[i].toKey,data[i].totalCount)
+	}
+	//子线的创建
 	function createLineChild(come,to,number){
-		var come_x=subscript(come);
-		console.log(come_x)
+		var obj={};
+		obj.come=lineSubscript(come);
+		obj.to=lineSubscript(to);
 		
-//		cav.beginPath();
-//     	cav.moveTo(0+a, 0+b);
-//     	cav.lineTo(100+a, 100+b);
-//		cav.stroke();
-		function subscript(obj){
-			var x=[],y=[],z=[x,y],j=0;//不确定有几条，须为数组
-			for(var i=0;i<data.length;i++){
-				if(dom[i].key==come){
-					x[j]=dom[i].x;
-					y[j]=dom[i].y;
-					j++;
-				}
-			}
-			return z;
-		}
+		
+		
+		
+		
+		cav.beginPath();
+       	cav.moveTo(obj.come.x,obj.come.y);
+       	cav.lineTo(obj.to.x,obj.to.y);
+		cav.stroke();
 		
 	}
-	
-
+}
+//line坐标校正
+function lineSubscript(obj){
+	var z={};
+	var dom=data.nodeInfo;
+	for(var i=0;i<dom.length;i++){
+		if(dom[i].key==obj){
+			var id=$('div[uid="'+dom[i].key+'"]');
+			z.x=dom[i].x+$(id).width()/2+10;
+			z.y=dom[i].y+$(id).height()/2+5;
+			return z;
+		}
+	}
 }
 //拖动监听
 function myTopoDrag(event,obj){
@@ -152,37 +214,53 @@ function myTopoDrag(event,obj){
 				e_r_c=0;
 			}
 		}
+		//*data为全局
 
+		var nodeInfo=data.nodeInfo;
+		var nodeLinkInfo=data.nodeLinkInfo;
 		
 		
-//		console.log(obj_n_r)
-		
-    	$(obj).css({top:e_x_t,left:e_y_l})
-//      var w=$(window).width()-$('#drag').width();
-//      var h=$(window).height()-$('#drag').height();
-//      if(t<0){
-//          t=0;
-//          }
-//      else if(t>h){
-//          t=h;
-//          }  
-//       if(l<0){
-//           l=0;
-//           }
-//       else if(l>w){
-//            l=w;      
-//           }
+		var objIndex=subscript($(obj),nodeInfo);
+		nodeInfo[objIndex].x=e_y_l;
+		nodeInfo[objIndex].y=e_x_t;
 
-  		
+		//牵动有关系的中央数值
+		var sel=getAllNumber($(obj).attr('uid'),nodeLinkInfo);
 
+		//牵动所有线
+		createLine(nodeLinkInfo,nodeInfo,sel);
 		
+
+
+    	$(obj).css({top:e_x_t,left:e_y_l});
 		
-		
-		
+
 	})
  	$(this).mouseup(function (event){
     	$(this).unbind("mousemove"); 
 	});
+	function subscript(obj,all){
+		var id=$(obj).attr('uid');
+		for(var i=0;i<all.length;i++)
+		if(all[i].key==id)return i;
+	}
+	function getAllNumber(obj,data){
+		var arr=[];
+		for(var i=0;i<data.length;i++){
+			if(data[i].fromKey==obj || data[i].toKey==obj){
+				var objMid=mid(lineSubscript(data[i].fromKey),lineSubscript(data[i].toKey));
+				var obj_c=$("small[uid='"+(data[i].fromKey+data[i].toKey)+"']");
+				var obj_w=$(obj_c).width();
+				var obj_h=$(obj_c).height();
+				$(obj_c).css({
+					top:objMid.y-obj_h/2,
+					left:objMid.x-obj_w/2
+				});
+				arr.push(i);
+			}
+		}
+		return arr;
+	}
 }
 
 
